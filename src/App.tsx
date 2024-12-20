90d0067614e2185b4b68122c7930539c979174e1
a1d0us <diakonenko.vanya@gmail.com>
2024-12-20 20:11:27 +0100

    react router part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Root from "./pages/Root.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import SearchPage from "./pages/SearchPage.tsx";
+import DetailsPage from "./pages/DetailsPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    children: [
+      {
+        index: true,
+        element: <HomePage/>
+      },
+      {
+        path: '/search',
+        element: <SearchPage/>,
+        loader: async ({request}) => {
+          const {searchParams} = new URL(request.url);
+          const term = searchParams.get('term');
+
+          if (!term) {
+            throw new Error('Search term must be provided');
+          }
+
+
+
+        },
+      },
+      {
+        path: '/packages/:name',
+        element: <DetailsPage/>
+      },
+    ]
+  }
+]);
+
 function App() {
 
   return (
-    <>
-      App
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,40 @@
+import {useState} from "react";
+import React from "react";
+import {useNavigate} from "react-router-dom";
+import {VscSearch} from "react-icons/vsc";
+
+// import {Form} from "react-router-dom";
+
+export default function SearchInput() {
+  const navigate = useNavigate();
+
+  const [term, setTerm] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    navigate(`/search?term=${term}`);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="relative">
+        <div className="absolute inset-y-0 flex items-center pl-3">
+          <VscSearch className="h-5 w-5 text-gray-500"/>
+        </div>
+        <input
+          className="pl-10 py-2 w-full border-0 shadow-none"
+          value={term}
+          onChange={e => setTerm(e.target.value)}
+          placeholder="Search packages..."
+        />
+      </div>
+    </form>
+  );
+
+  // return (
+  //   <Form action="/search">
+  //     <input name="term"/>
+  //   </Form>
+  // );
+}
